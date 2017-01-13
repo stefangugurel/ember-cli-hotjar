7feaf090ae3b374202604847b522829a5e9b7def
Stefan Gugurel <stefangugurel@gmail.com>
2017-01-13 12:05:16 +0200

    Add hotjar tracking code.

--- index.js
+++ index.js
@@ -2,5 +2,20 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-hotjar'
+  name: 'ember-cli-hotjar',
+
+  contentFor: function(type, config) {
+    if (type === 'body-footer' && config.hotjar) {
+      return '<script>' +
+      '(function(h,o,t,j,a,r){' +
+        'h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};' +
+        'h._hjSettings={hjid:' + config.hotjar.siteId + ',hjsv:5};' +
+        'a=o.getElementsByTagName("head")[0];' +
+        'r=o.createElement("script");r.async=1;' +
+        'r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;' +
+        'a.appendChild(r);' +
+      '})(window,document,"//static.hotjar.com/c/hotjar-",".js?sv=");' +
+      '</script>';
+    }
+  }
 };
